09e6d02a0125fced1911a23eab708a9d5bccb7df
Hamadé <hamandea@gmail.com>
2022-03-31 13:22:51 +0000

    Ajout

--- Src/settings.py
+++ Src/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT =  os.path.join(BASE_DIR,"staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR,"Src/static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"Src/static")
 ]
